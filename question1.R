22fd012647ee6fdf600ca6835a5b10b353e10053
robotNoir <Angus@Anguss-iMac.local>
2017-07-26 09:34:46 +1000

    updated 26.07.17 9:30am

--- question1.R
+++ question1.R
@@ -1,149 +1,89 @@
+# ------------------------------
 # Case Study 1 - Study of Graft Arteries
 # Angus Robertson
 # 22 July 2017
-# Packages: data.table, 
+# Packages: 
+# ------------------------------
 
-library(data.table)
+library(data.table, ggplot2)
 
-# Import data file from csv and clean data
-data <- read.csv('artery030510.csv', header=T)
+# Import and fix graft.arteries data
+graft.arteries <- read.csv('artery030510.csv', header=T)
 
-  #Remove unused columns and rows
-  data <- subset(data, select = -c(X, X.1, X.2, X.3))
-  data <- data[!apply(is.na(data) | data == '', 1, all), ]
+  graft.arteries <- subset(graft.arteries, select = -c(X, X.1, X.2, X.3))
+  graft.arteries <- graft.arteries[!apply(is.na(graft.arteries) | data == '', 1, all), ]
+  colnames(graft.arteries)[which(names(graft.arteries) == "RA...luminal.narrowing")] <- 'RA.Luminal.narrowing.percent'
+  colnames(graft.arteries)[which(names(graft.arteries) == "RA..Intimal.thickness.index")] <- 'RA.Intimal.thickness.index'
+  colnames(graft.arteries)[which(names(graft.arteries) == "ITA...luminal.narrowing")] <- 'ITA.Luminal.narrowing.percent'
+  colnames(graft.arteries)[which(names(graft.arteries) == "ITA..Intimal.thickness.index")] <- 'ITA.Intimal.thickness.index'
+  colnames(graft.arteries)[which(names(graft.arteries) == "RA.DLI")] <- 'RA.DLI.mm'
+  colnames(graft.arteries)[which(names(graft.arteries) == "RA.IEL.area")] <- 'RA.IEL.area.mm2'
+  colnames(graft.arteries)[which(names(graft.arteries) == "RA.Intimal.area")] <- 'RA.Intimal.area.mm2'
+  colnames(graft.arteries)[which(names(graft.arteries) == "RA.Medial.area")] <- 'RA.Medial.area.mm2'
+  colnames(graft.arteries)[which(names(graft.arteries) == "RA.Intimal.width")] <- 'RA.Intimal.width.mm'
+  colnames(graft.arteries)[which(names(graft.arteries) == "RA.Medial.width")] <- 'RA.Medial.width.mm'
+  colnames(graft.arteries)[which(names(graft.arteries) == "ITA.DLI")] <- 'ITA.DLI.mm'
+  colnames(graft.arteries)[which(names(graft.arteries) == "ITA.IEL.area")] <- 'ITA.IEL.area.mm2'
+  colnames(graft.arteries)[which(names(graft.arteries) == "ITA.Intimal.area")] <- 'ITA.Intimal.area.mm2'
+  colnames(graft.arteries)[which(names(graft.arteries) == "ITA.Medial.area")] <- 'ITA.Medial.area.mm2'
+  colnames(graft.arteries)[which(names(graft.arteries) == "ITA.Intimal.width")] <- 'ITA.Intimal.width.mm'
+  colnames(graft.arteries)[which(names(graft.arteries) == "ITA.Medial.width")] <- 'ITA.Medial.width.mm'
+  graft.arteries$Age                     <- as.numeric(graft.arteries$Age)
+  graft.arteries$Gender                  <- factor(graft.arteries$Gender, levels=c(0:1), labels=c('female', 'male'))
+  graft.arteries$Diabetes                <- factor(graft.arteries$Diabetes, levels=c(0:1), labels=c('no', 'yes'))
+  graft.arteries$Ever.smoked             <- factor(graft.arteries$Ever.smoked, levels=c(0:1), labels=c('no', 'yes'))
+  graft.arteries$PVD                     <- factor(graft.arteries$PVD, levels=c(0:1), labels=c('no', 'yes'))
+  graft.arteries$CVD                     <- factor(graft.arteries$CVD, levels=c(0:1), labels=c('no', 'yes'))
+  graft.arteries$Hypertension            <- factor(graft.arteries$Hypertension, levels=c(0:1), labels=c('no', 'yes'))
+  graft.arteries$RA.medial.calcification <- factor(graft.arteries$RA.medial.calcification, levels=c(0:1), labels=c('no', 'yes'))
+  graft.arteries$RA.intimal.abnormality  <- factor(graft.arteries$RA.intimal.abnormality, levels=c(0:2), labels=c('normal', 'intimal thickening', 'atherosclerosis'))
+  graft.arteries$ITA.intimal.abnormality <- factor(graft.arteries$ITA.intimal.abnormality, levels=c(0:2), labels=c('normal', 'intimal thickening', 'atherosclerosis'))
   
-  #Fix column names
-  setnames(data, old=c("RA...luminal.narrowing",         
-                       "RA..Intimal.thickness.index",
-                       "RA.Intima.to.media.ratio",     
-                       "ITA...luminal.narrowing",        
-                       "ITA..Intimal.thickness.index",    
-                       "ITA.Intima.to.media.ratio",    
-                       "RA.DLI",       
-                       "RA.IEL.area",                      
-                       "RA.Intimal.area",                  
-                       "RA.Medial.area",                   
-                       "RA.Intimal.width",                 
-                       "RA.Medial.width",                  
-                       "ITA.DLI",                      
-                       "ITA.IEL.area",                     
-                       "ITA.Intimal.area",                 
-                       "ITA.Medial.area",                
-                       "ITA.Intimal.width",                
-                       "ITA.Medial.width"), 
-                new=c("RA.Luminal.narrowing.(%)",
-                      "RA.Intimal.thickness.index",
-                      "RA.Intima-to-media.ratio",
-                      "ITA.Luminal.narrowing.(%)",
-                      "ITA.Intimal.thickness.index",
-                      "ITA.Initma-to-media.ratio",
-                      "RA.DLI.(mm)",
-                      "RA.IEL.area.(mm^2)",
-                      "RA.Intimal.area.(mm^2)",
-                      "RA.Medial.area.(mm^2)",                   
-                      "RA.Intimal.width.(mm)",                 
-                      "RA.Medial.width.(mm)",                  
-                      "ITA.DLI.(mm)",                      
-                      "ITA.IEL.area.(mm^2)",                     
-                      "ITA.Intimal.area.(mm^2)",                 
-                      "ITA.Medial.area.(mm^2)",                
-                      "ITA.Intimal.width.(mm)",                
-                      "ITA.Medial.width.(mm)"
-                      ))
+  write.csv(graft.arteries, 'graft.arteries.cleaned.CSV.csv')
   
-  #Fix data types
-  data$Age <- as.numeric(data$Age)
-
-  data$Gender <- as.factor(data$Gender)
-  data$Diabetes <- as.factor(data$Diabetes)
-  data$Ever.smoked <- as.factor(data$Ever.smoked)
-  data$PVD <- as.factor(data$PVD)
-  data$CVD <- as.factor(data$CVD)
-  data$Hypertension <- as.factor(data$Hypertension)
-  data$RA.medial.calcification <- as.factor(data$RA.medial.calcification)
-  data$ITA.intimal.abnormality <- as.factor(data$ITA.intimal.abnormality)
-  data$RA.intimal.abnormality <- as.factor(data$RA.intimal.abnormality)
-  
-  levels(data$Gender) <- c('female', 'male')
-  levels(data$Diabetes) <- c('no', 'yes')
-  levels(data$Ever.smoked) <- c('no', 'yes')
-  levels(data$PVD) <- c('no', 'yes')
-  levels(data$CVD) <- c('no', 'yes')
-  levels(data$Hypertension) <- c('no', 'yes')
-  levels(data$RA.medial.calcification) <- c('no', 'yes')
-  levels(data$RA.intimal.abnormality) <- c('normal', 'intimal thickening', 'atherosclerosis')
-  levels(data$ITA.intimal.abnormality) <- c('normal', 'intimal thickening', 'atherosclerosis')
-  
-  #Export fixed csv 
-  write.csv(data, 'question1CSV.csv')
-  
-  # --------------------------------------------------------------------------------------------
-  
-  # part (b) - boxplot display of RA luminal narrowing, intimal thickness and intima-media ratio depending on ever smoked
-  par(mfrow=c(3,1))
-  boxplot(data$"RA.Luminal.narrowing.(%)" ~ data$Ever.smoked, 
-          data=data,
-          main="Radial Artery Luminal Narrowing for Smokers vs. Non-Smokers",
-          ylab="Smoker",
-          xlab="Radial Artery Luminal Narrowing (%)",
-          col="grey",
-          ylim = c(0, 70),
-          pars = list(xaxp = c(0, 70, 14)),
-          horizontal=T)
-  grid(nx = NULL, ny= NA)
-  boxplot(data$RA.Intimal.thickness.index ~ data$Ever.smoked, 
-          data=data,
-          main="Radial Artery Intimal Thickness Index for Smokers vs. Non-Smokers",
-          ylab="Smoker",
-          xlab="Radial Artery Intimal Thickness Index",
-          col="grey",
-          ylim = c(0, 2),
-          pars = list(xaxp = c(0, 2, 10)),
-          horizontal=T)
-  grid(nx = NULL, ny= NA)
-  boxplot(data$"RA.Intima-to-media.ratio" ~ data$Ever.smoked, 
-          data=data,
-          main="Radial Artery Intima-to-media Ratio for Smokers vs. Non-Smokers",
-          ylab="Smoker",
-          xlab="Radial Artery Intima-to-media Ratio",
-          col="grey",
-          ylim = c(0, 12),
-          pars = list(xaxp = c(0, 12, 12)),
-          horizontal=T)
-  grid(nx = NULL, ny= NA)
-  par(mfrow=c(1,1))
-  
-  # part (c) - boxplot display of 
-  t.test(data$"RA.Luminal.narrowing.(%)" ~ data$Ever.smoked, var.equal=T, conf.level=0.95)
-  t.test(data$RA.Intimal.thickness.index ~ data$Ever.smoked, var.equal=T, conf.level=0.95)
-  t.test(data$"RA.Intima-to-media.ratio" ~ data$Ever.smoked, var.equal=T, conf.level=0.95)
-  
-  plot1 <- ggplot(data, aes(Ever.smoked,RA.Luminal.narrowing....)) + 
+# Generate plots for question 1
+  plot1.1 <- ggplot(graft.arteries, aes(Ever.smoked,RA.Luminal.narrowing.percent)) + 
     geom_boxplot() + 
     coord_flip() +
-    theme_light() +
+    theme_bw(base_size = 8) +
+    scale_x_discrete(labels=c("No","Yes")) +
+    scale_y_continuous(breaks = seq(0, 70, 10), limits=c(0,70), expand=c(0,0)) +
     labs(
-      title = "Comparison of Radial Artery Main Indicies for Smokers and Non-Smokers",
-      y="Intima-to-Media Ratio", 
-      x="Patient History of Smoking")
+      y="Luminal Narrowing (%)", 
+      x="Smoker?")
+  plot1.1
+  ggsave("plot1.1.tiff", height=4.6, width=17, units="cm", dpi=600)
   
-  plot2 <- ggplot(data, aes(Ever.smoked,RA.Intimal.thickness.index)) + 
+  plot1.2 <- ggplot(graft.arteries, aes(Ever.smoked,RA.Intimal.thickness.index)) + 
     geom_boxplot() + 
     coord_flip() +
-    theme_light() +
+    theme_bw(base_size = 8) +
+    scale_x_discrete(labels=c("No","Yes")) +
+    scale_y_continuous(breaks = seq(0, 2, 0.2), limits=c(0,2), expand=c(0,0)) +
     labs(
-      title = "Comparison of Radial Artery Main Indicies for Smokers and Non-Smokers", 
-      y="Intima-to-Media Ratio", 
-      x="Patient History of Smoking")
+      y="Intimal Thickness Index", 
+      x="Smoker?")
+  plot1.2
+  ggsave("plot1.2.tiff", height=4.6, width=17, units="cm", dpi=600)
   
-  plot3 <- ggplot(data, aes(Ever.smoked,RA.Intima.to.media.ratio)) + 
+  plot1.3 <- ggplot(graft.arteries, aes(Ever.smoked,RA.Intima.to.media.ratio)) + 
     geom_boxplot() + 
     coord_flip() +
-    theme_light() +
+    theme_bw(base_size = 8) +
+    scale_x_discrete(labels=c("No","Yes")) +
+    scale_y_continuous(breaks = seq(0, 11, 1), limits=c(0,11), expand=c(0,0)) +
     labs(
-      title = "Comparison of Radial Artery Main Indicies for Smokers and Non-Smokers",
       y="Intima-to-Media Ratio", 
-      x="Patient History of Smoking")
+      x="Smoker?")
+  plot1.3
+  ggsave("plot1.3.tiff", height=4.6, width=17, units="cm", dpi=600)
   
-  grid.arrange(plot1, plot2, plot3, ncol=1)
+# Compute t-test's for question 1
+  t.test.1.1 <- t.test(graft.arteries$RA.Luminal.narrowing.percent ~ graft.arteries$Ever.smoked, var.equal=T, conf.level=0.95)
+  t.test.1.2 <- t.test(graft.arteries$RA.Intimal.thickness.index ~ graft.arteries$Ever.smoked, var.equal=T, conf.level=0.95)
+  t.test.1.3 <- t.test(graft.arteries$RA.Intima.to.media.ratio ~ graft.arteries$Ever.smoked, var.equal=T, conf.level=0.95)
+  
+  
+  
+
   
--- question2.R
+++ question2.R
@@ -23,35 +23,44 @@ trunk.diameter <- read.csv('winery_datasheet/Trunk diameter-Table 1.csv', header
   write.csv(trunk.diameter, file='winery_datasheet/trunk.diameter.cleaned.CSV.csv')
 
 # Import and fix harvest yield data  
-harvest.yield  <- read.csv('winery_datasheet/Harvest yield-Table 1.csv', header=T)
+harvest.yield <- read.csv('winery_datasheet/harvest.yield.corrected.csv', header=T)
   harvest.yield$Treatment <- factor(harvest.yield$Treatment, levels=c(1:3), labels=c('Herbicide', 'Compost', 'Straw'))
   harvest.yield$EWblock   <- factor(harvest.yield$EWblock, levels=c(1:3), labels=c('Block 1', 'Block 2', 'Block 3'))
   harvest.yield$Aspect    <- factor(harvest.yield$Aspect, levels=c(1:2), labels=c('North', 'South'))
   harvest.yield$Block     <- factor(harvest.yield$Block, levels=c(1:6), labels=c('Block 1, South', 'Block 1, North', 'Block 2, South', 'Block 2, North', 'Block 3, South', 'Block 3, North'))
-  colnames(harvest.yield)[which(names(harvest.yield) == "Number.of.bunches.per.vine..2001.")] <- 'Number.of.bunches.per.vine.2001'
-  colnames(harvest.yield)[which(names(harvest.yield) == "Number.of.bunches.per.vine..2002.")] <- 'Number.of.bunches.per.vine.2002'
-  colnames(harvest.yield)[which(names(harvest.yield) == "Number.of.bunches.per.vine..2003.")] <- 'Number.of.bunches.per.vine.2003'
-  colnames(harvest.yield)[which(names(harvest.yield) == "Weight.of.bunches.per.vine.in.kg..2001.")] <- 'Weight.of.bunches.per.vine.2001.kg'
-  colnames(harvest.yield)[which(names(harvest.yield) == "Weight.of.bunches.per.vine.in.kg..2002.")] <- 'Weight.of.bunches.per.vine.2002.kg'
-  colnames(harvest.yield)[which(names(harvest.yield) == "Weight.of.bunches.per.vine.in.kg..2003.")] <- 'Weight.of.bunches.per.vine.2003.kg'
-  colnames(harvest.yield)[which(names(harvest.yield) == "Weight.of.material.pruned.per.vine.in.kg..2001.")] <- 'Weight.of.material.pruned.per.vine.2001.kg'
-  colnames(harvest.yield)[which(names(harvest.yield) == "Weight.of.material.pruned.per.vine.in.kg..2002.")] <- 'Weight.of.material.pruned.per.vine.2002.kg'
-  colnames(harvest.yield)[which(names(harvest.yield) == "Weight.of.material.pruned.per.vine.in.kg..2003.")] <- 'Weight.of.material.pruned.per.vine.2003.kg'
+  harvest.yield$Year      <- as.factor(harvest.yield$Year)
+  colnames(harvest.yield)[which(names(harvest.yield) == "Number.of.bunches.per.vine..2001.")] <- 'Number.of.bunches.per.vine'
+  colnames(harvest.yield)[which(names(harvest.yield) == "Weight.of.bunches.per.vine.in.kg..2001.")] <- 'Weight.of.bunches.per.vine.kg'
+  colnames(harvest.yield)[which(names(harvest.yield) == "Weight.of.material.pruned.per.vine.in.kg..2001.")] <- 'Weight.of.material.pruned.per.vine.kg'
   
   write.csv(harvest.yield, file='winery_datasheet/harvest.yield.cleaned.CSV.csv')
   
-  plot2.1 <- ggplot(data=harvest.yield) +
-              geom_point(aes(x=Number.of.bunches.per.vine.2001, y=Weight.of.bunches.per.vine.2001.kg, group='2001', shape="2001")) +
-              geom_point(aes(x=Number.of.bunches.per.vine.2002, y=Weight.of.bunches.per.vine.2002.kg, group='2002', shape="2002")) +
-              geom_point(aes(x=Number.of.bunches.per.vine.2003, y=Weight.of.bunches.per.vine.2003.kg, group='2003', shape="2003")) +
-              scale_shape_manual("Year",
-                                 breaks = c("2001", "2002", "2003"),
-                                 values = c(1:3)) +
+# Graph to show relationship between average number of bunches harvested and average weight harvested for 2002
+  plot2.1 <- ggplot(data=subset(harvest.yield, Year == "2002"), aes(x=Number.of.bunches.per.vine, y=Weight.of.bunches.per.vine.kg)) +
+              geom_point(shape=1) +
               theme_bw(base_size = 12) +
               scale_x_continuous(breaks = seq(0, 50, 5), limits=c(0,50), expand=c(0,0)) +
-              scale_y_continuous(breaks = seq(1, 7, 1), limits=c(0,7), expand=c(0,0)) +
+              scale_y_continuous(breaks = seq(0, 3, 0.5), limits=c(0,3), expand=c(0,0)) +
               labs(
                 y="Weight of Bunches per vine (kg)", 
                 x="Number of Bunches per vine")
   plot2.1
   ggsave("plot2.1.tiff", height=11.22, width=17, units="cm", dpi=600)
+  
+  model2.1 <- lm(formula = Weight.of.bunches.per.vine.kg ~ Number.of.bunches.per.vine, data=subset(harvest.yield, Year == "2002"))
+  summary(model2.1)
+  confint(model2.1, level=0.95)
+  
+  model2.2 <- lm(formula = Weight.of.bunches.per.vine.kg ~ Number.of.bunches.per.vine*Year, data=harvest.yield)
+  summary(model2.2)
+  
+  ggplot(data=harvest.yield, aes(x=Number.of.bunches.per.vine, y=Weight.of.bunches.per.vine.kg, group=Year)) +
+    geom_point(aes(shape=as.factor(Year))) +
+    scale_shape_manual(values=c(1:3)) +
+    theme_bw(base_size = 12) +
+    scale_x_continuous(breaks = seq(0, 50, 5), limits=c(0,50), expand=c(0,0)) +
+    scale_y_continuous(breaks = seq(0, 7, 1), limits=c(0,3), expand=c(0,0)) +
+    labs(
+      y="Weight of Bunches per vine (kg)", 
+      x="Number of Bunches per vine")
+  ggsave("plot2.2.tiff", height=11.22, width=17, units="cm", dpi=600)
